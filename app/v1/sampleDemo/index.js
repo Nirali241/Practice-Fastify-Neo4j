0c5468686663790d031b77801c117304fa611490
Administrator <administrator@Administrators-MacBook-Pro.local>
2021-06-18 17:11:15 +0530

    commit

--- app/v1/sampleDemo/index.js
+++ app/v1/sampleDemo/index.js
@@ -18,44 +18,37 @@ const node = new Nodes(fastify);
     // schema: sampleSchema,
     handler: node.createDepartment.bind(node)
   });
-
+ 
   fastify.route({
     method: "POST",
-    url: "/department/:name",
+    url: "/employee/:empname/:relationship/:type/:entity",
     // schema: sampleSchema,
-    handler: node.setAttributeValue.bind(node)
+    handler: node.createRelationshipwithNode.bind(node)
   });
 
+  
   fastify.route({
-    method: "POST",
-    url: "/employee/:empname/:relationship/:depname",
-    // schema: sampleSchema,
-    handler: node.setAttributeValueGlobally.bind(node)
-  });
-
-  fastify.route({
-    method: "POST",
-    url: "/employeesecond/:empname/:relationship/:emp",
-    // schema: sampleSchema,
-    handler: node.setAttributeValuesecondlevel.bind(node)
+    method: "GET",
+    url: "/employee/:department",
+    handler: node.getEmployeeValue.bind(node)
   });
 
   fastify.route({
     method: "GET",
-    url: "/employeelist/:department",
-    handler: node.getAttributesValue.bind(node)
+    url: "/employee?salary=",
+    handler: node.getEmployeeSalary.bind(node)
   });
 
   fastify.route({
-    method:"GET",
-    url:"/employee/:salary",
-    handler: node.getAttributesSalary.bind(node)
-  });
-
-  fastify.route({
-    method:"PATCH",
-    url:"/employee/:update",
+    method: "PATCH",
+    url: "/employee/:name",
     handler: node.getUpdateAttributeValue.bind(node)
   });
+
+  fastify.route({
+    method: "PATCH",
+    url: "/employee/:empname/:relation/:depname",
+    handler: node.getupdateEmployeeRelation.bind(node)
+  })
 
 };
